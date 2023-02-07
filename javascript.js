55fa3316cacf3e1bc1532dac9366c2de3c3c66c0
Haley Sorensen <sorensenhaley@gmail.com>
2023-02-07 12:13:13 -0700

    Put selections and playround into game function loops 5 times.

--- javascript.js
+++ javascript.js
@@ -1,48 +1,54 @@
-let playerSelection = prompt ("Rock, Paper or Scissors?",'');
+function game() {
 
-    if (playerSelection.toLowerCase() == "rock"){
-        playerSelection = "Rock";
-    } else if (playerSelection.toLowerCase()== "paper"){
-        playerSelection = "Paper";
-    } else if(playerSelection.toLowerCase()== "scissors"){
-        playerSelection = "Scissors";
-    } else if (playerSelection.toLowerCase() != "rock" || "paper" || "scissors") {
-        playerSelection = "Not a valid answer.";
+    let playerSelection = prompt ("Rock, Paper or Scissors?",'');
+
+        if (playerSelection.toLowerCase() == "rock"){
+            playerSelection = "Rock";
+        } else if (playerSelection.toLowerCase()== "paper"){
+            playerSelection = "Paper";
+        } else if(playerSelection.toLowerCase()== "scissors"){
+            playerSelection = "Scissors";
+        } else if (playerSelection.toLowerCase() != "rock" || "paper" || "scissors") {
+            playerSelection = "Not a valid answer.";
+        }
+
+    console.log(playerSelection);
+
+    function getComputerChoice() {
+        return Math.random();
     }
 
-console.log(playerSelection);
+    getComputerChoice();
 
-function getComputerChoice() {
-    return Math.random();
-}
+    let computerSelection = getComputerChoice();
+        if (getComputerChoice() < 0.33) {
+            computerSelection = "Rock";
+        } else if (getComputerChoice() <= 0.66) {
+            computerSelection = "Paper";
+        } else {
+            computerSelection = "Scissors";
+        }
+    
+    console.log(computerSelection);
 
-getComputerChoice();
-
-let computerSelection = getComputerChoice();
-    if (getComputerChoice() < 0.33) {
-        computerSelection = "Rock";
-    } else if (getComputerChoice() <= 0.66) {
-        computerSelection = "Paper";
-    } else {
-        computerSelection = "Scissors";
+    function playRound(playerSelection, computerSelection) {
+        if (playerSelection == "Rock" && computerSelection == "Scissors") {
+            return "You Won! Rock beats Scissors.";
+        } else if (playerSelection == "Rock" && computerSelection == "Paper") {
+            return "You Lose. Paper beats Rock.";
+        } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+            return "You Lose. Rock beats Scissors.";
+        } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+            return "You Won! Scissors beats Paper.";
+        } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+            return "You Won! Paper beats Rock.";
+        } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+            return "You Lose. Scissors beats Paper.";
+        } else {return "It's a tie."}
     }
-
-console.log (computerSelection);
-
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection == "Rock" && computerSelection == "Scissors") {
-        return "You Won! Rock beats Scissors.";
-    } else if (playerSelection == "Rock" && computerSelection == "Paper") {
-        return "You Lose. Paper beats Rock.";
-    } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
-        return "You Lose. Rock beats Scissors.";
-    } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
-        return "You Won! Scissors beats Paper.";
-    } else if (playerSelection == "Paper" && computerSelection == "Rock") {
-        return "You Won! Paper beats Rock.";
-    } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
-        return "You Lose. Scissors beats Paper.";
-    } else {return "It's a tie."}
+    console.log(playRound(playerSelection, computerSelection));
 }
-
-console.log(playRound(playerSelection, computerSelection));
+    for (let i = 0; i < 4; i++){
+        game();
+    }
+    console.log(game());
